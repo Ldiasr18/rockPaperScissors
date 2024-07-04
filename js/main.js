397eac722eb5d2e8ed7b37576b70ba3da767e71e
Lucas <ldiasr18@gmail.com>
2024-07-04 09:27:56 -0300

    added playRound function

--- js/main.js
+++ js/main.js
@@ -1,10 +1,11 @@
-function getComputerChoice(){
-    let rand;   
+//functions
+function getComputerChoice() {
+    let rand;
     rand = Math.floor(Math.random() * 3);
-    switch (rand){
+    switch (rand) {
         case 0:
             return "Rock";
-            
+
         case 1:
             return "Paper";
 
@@ -14,36 +15,59 @@ function getComputerChoice(){
     }
 }
 
-function getHumanChoice(){
+function getHumanChoice() {
     let playerChoice;
     playerChoice = prompt("Enter your choice: Rock, Paper or Scissors");
+    //make sure the choice is valid and standardize the return
     playerChoice = playerChoice.toLowerCase();
-    if (playerChoice === "rock"){
+    if (playerChoice === "rock") {
         return "Rock";
-    }else if (playerChoice === "paper"){
+    } else if (playerChoice === "paper") {
         return "Paper";
-    }else if(playerChoice === "scissors"){
+    } else if (playerChoice === "scissors") {
         return "Scissors";
-    }else{
+    } else {
         return -1;
     }
 }
 
+function playRound(humanChoice, computerChoice){
+    if (humanChoice === computerChoice){
+        console.log("It's a draw!");
+    }else if (humanChoice === "Rock" && computerChoice === "Scissor"){
+        console.log("You WIN! Rock beats Scissors!");
+    }else if (humanChoice === "Paper" && computerChoice === "Rock"){
+        console.log("You WIN! Paper beats rock!");
+    }else if (humanChoice === "Scissors" && computerChoice === "Paper"){
+        console.log("You WIN! Scissors beats paper!");
+    }else{
+        console.log(`You LOSE! ${computerChoice} beats ${humanChoice}!`);
+    }
+}
+
+//end functions
+
+
 let computerChoice, humanChoice;
+let computerScore = 0, humanScore = 0;
+
 console.log("hello world!")
-//functions to get choices
+//functions call to get choices
 computerChoice = getComputerChoice();
 humanChoice = getHumanChoice();
 
 //function to check the human choice, and if it's invalid asks to enter again the value.
-while (humanChoice == -1){
+while (humanChoice == -1) {
     console.log("choice is invalid \n please enter a valid choice: Rock, paper or scissors");
     humanChoice = getHumanChoice();
-//to do:
-//make an exit value if the player do not want to play anymore 
+    //to do:
+    //make an exit value if the player do not want to play anymore 
 }
 
+console.log(computerChoice + "computer");
+console.log(humanChoice + "player");
 
-console.log(computerChoice);
-console.log(humanChoice);
+playRound(humanChoice, computerChoice);
+
+
 
