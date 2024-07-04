7411d846f8bce7c463897d1d67215d56019fc4e4
Lucas <ldiasr18@gmail.com>
2024-07-04 10:40:10 -0300

    made the playGame function, and moved/reworked the other functions to work with it

--- js/main.js
+++ js/main.js
@@ -16,6 +16,7 @@ function getComputerChoice() {
 }
 
 function getHumanChoice() {
+    while(true){
     let playerChoice;
     playerChoice = prompt("Enter your choice: Rock, Paper or Scissors");
     //make sure the choice is valid and standardize the return
@@ -25,49 +26,56 @@ function getHumanChoice() {
     } else if (playerChoice === "paper") {
         return "Paper";
     } else if (playerChoice === "scissors") {
+        console.log(playerChoice);
         return "Scissors";
     } else {
-        return -1;
+        //function to check the human choice, and if it's invalid asks to enter again the value.
+            console.log("choice is invalid \n please enter a valid choice: Rock, paper or scissors");
+            //to do:
+            //make an exit value if the player do not want to play anymore 
+        }
     }
 }
 
-function playRound(humanChoice, computerChoice){
-    if (humanChoice === computerChoice){
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === computerChoice) {
         console.log("It's a draw!");
-    }else if (humanChoice === "Rock" && computerChoice === "Scissor"){
+    } else if (humanChoice === "Rock" && computerChoice === "Scissors") {
         console.log("You WIN! Rock beats Scissors!");
-    }else if (humanChoice === "Paper" && computerChoice === "Rock"){
+        humanScore++;
+    } else if (humanChoice === "Paper" && computerChoice === "Rock") {
         console.log("You WIN! Paper beats rock!");
-    }else if (humanChoice === "Scissors" && computerChoice === "Paper"){
+        humanScore++;
+    } else if (humanChoice === "Scissors" && computerChoice === "Paper") {
         console.log("You WIN! Scissors beats paper!");
-    }else{
+        humanScore++;
+    } else {
         console.log(`You LOSE! ${computerChoice} beats ${humanChoice}!`);
+        computerScore++;
+    }
+}
+
+function playGame(){
+    let computerChoice, humanChoice;
+    console.log("teste");
+    for(let i = 0; i < 5; i++) {
+        computerChoice = getComputerChoice();
+        humanChoice = getHumanChoice();
+        playRound(humanChoice, computerChoice);
+        console.log(`Score:
+            Player: ${humanScore}
+            Computer: ${computerScore}`);
+    }
+    if(humanScore > computerScore){
+        console.log("you won The game!");
+    }else{
+        console.log("Computer won the game!")
     }
 }
 
 //end functions
 
+//main
 
-let computerChoice, humanChoice;
 let computerScore = 0, humanScore = 0;
-
-console.log("hello world!")
-//functions call to get choices
-computerChoice = getComputerChoice();
-humanChoice = getHumanChoice();
-
-//function to check the human choice, and if it's invalid asks to enter again the value.
-while (humanChoice == -1) {
-    console.log("choice is invalid \n please enter a valid choice: Rock, paper or scissors");
-    humanChoice = getHumanChoice();
-    //to do:
-    //make an exit value if the player do not want to play anymore 
-}
-
-console.log(computerChoice + "computer");
-console.log(humanChoice + "player");
-
-playRound(humanChoice, computerChoice);
-
-
-
+playGame();
