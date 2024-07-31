b68ccb7e75f8b2df3d4fb72c6a4a7fc6882d2337
Ldiasr18 <ldiasr18@gmail.com>
2024-07-31 12:35:37 -0300

    fixed update score, added resetGame function and added conditions for the winner of the game

--- js/main.js
+++ js/main.js
@@ -1,71 +1,74 @@
+const humanElement = document.getElementById("humanScore");
+const computerElement = document.getElementById("computerScore");
+const drawElement = document.getElementById("drawScore");
+
+let drawScore = 0;
+let playerScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
-    let rand;
-    rand = Math.floor(Math.random() * 3);
-    switch (rand) {
-        case 0:
-            return "Rock";
-
-        case 1:
-            return "Paper";
-
-        case 2:
-            return "Scissors";
-
-    }
+  let rand;
+  rand = Math.floor(Math.random() * 3);
+  switch (rand) {
+    case 0:
+      return "Rock";
+    case 1:
+      return "Paper";
+    case 2:
+      return "Scissors";
+  }
 }
 
 function playGame() {
-    const playerChoice = document.querySelectorAll("button");
-    playerChoice.forEach((button) => {
-        button.addEventListener("click", () => {
-            let humanChoice = button.id;
-            let computerChoice = getComputerChoice();
-            playRound(humanChoice, computerChoice);
-        });
+  const playerChoice = document.querySelectorAll("button");
+  playerChoice.forEach((button) => {
+    button.addEventListener("click", () => {
+      let humanChoice = button.id;
+      let computerChoice = getComputerChoice();
+      playRound(humanChoice, computerChoice);
     });
+  });
 }
 
 function playRound(humanChoice, computerChoice) {
-    let winner = " ";
-
-    if (humanChoice === computerChoice) {
-        alert("It's a draw!");
-    } else if (humanChoice === "Rock" && computerChoice === "Scissors") {
-        alert("You WIN! Rock beats Scissors!");
-        winner = "human";
-    } else if (humanChoice === "Paper" && computerChoice === "Rock") {
-        alert("You WIN! Paper beats Rock!");
-        winner ="human";
-    } else if (humanChoice === "Scissors" && computerChoice === "Paper") {
-        alert("You WIN! Scissors beats Paper!");
-        winner = "human";
-    } else {
-        alert(`You LOSE! ${computerChoice} beats ${humanChoice}!`);
-        winner = "computer";
-    }
-    addScore(winner);
+  if (humanChoice === computerChoice) {
+    alert("It's a draw!");
+    drawScore++;
+  } else if (humanChoice === "Rock" && computerChoice === "Scissors") {
+    alert("You WIN! Rock beats Scissors!");
+    playerScore++;
+  } else if (humanChoice === "Paper" && computerChoice === "Rock") {
+    alert("You WIN! Paper beats Rock!");
+    playerScore++;
+  } else if (humanChoice === "Scissors" && computerChoice === "Paper") {
+    alert("You WIN! Scissors beats Paper!");
+    playerScore++;
+  } else {
+    alert(`You LOSE! ${computerChoice} beats ${humanChoice}!`);
+    computerScore++;
+  }
+  if(computerScore == 5 || playerScore == 5){
+    return resetGame();
+  }else{
+  updateScore();
 }
- 
-function addScore(winner){
-    alert(winner);
-    const humanScore = document.getElementById("humanScore");
-    const computerScore = document.getElementById("computerScore");
-    const drawScore = document.getElementById("drawScore"); 
-
-    switch(winner){
-        case "human":
-            humanScore.innerText = String(parseInt(humanScore.innerText) + 1);
-            break;
-        case "computer":
-            computerScore.innerText = String(parseInt(computerScore.innerText) + 1);
-            break;
-        default:
-            drawScore.innerText = String(parseInt(drawScore.innerText) + 1);
-            break;
-        }
-
-
 }
+
+function updateScore() {
+  humanElement.innerText = playerScore;
+  computerElement.innerText = computerScore;
+  drawElement.innerText = drawScore;
+}
+
+function resetGame(){
+  alert("Winner: " + (computerScore === 5 ? "Computer" : "Player"));
+  computerScore = 0;
+  playerScore = 0;
+  drawScore = 0;
+  updateScore();
+  
+}
+
 
 
 playGame();
